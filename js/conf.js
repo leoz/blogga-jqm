899000ce0ca1e843f0d265971553e61f6d3e56d3
leoz2011 <leoz@yahoo.com>
2014-04-28 22:59:30 -0400

    Fix initial date

--- js/conf.js
+++ js/conf.js
@@ -2,23 +2,22 @@
 // Configuration settings
 
 window.lj_conf = {
+    format : 'yyyy-MM-dd HH:mm:ss',
     number : 10,
     count : 10,
     date : null,
     expanded : false,
     journal : null,
+    setCurrentDate: function() {
+        this.date = $.format.date(new Date(), this.format);
+    },
     reset: function () {
+        this.setCurrentDate();
         this.number = 10;
         this.count = 10;
-		this.date = '';
-//        this.expanded = true;
-//        this.journal = 'toronto-ru';
     },
     setJournal: function(journal) {
-        this.number = 10;
-        this.count = 10;
-		this.date = '';
-//		this.expanded = true;
+        this.reset();
         this.journal = journal;
     }
 };
